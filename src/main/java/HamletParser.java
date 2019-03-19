0173ace2f748797cd486312ed522b124ca046a59
Sean MacLaughlin <seanmaclaughlin@zipcoders-MacBook-Pro.local>
2019-03-19 17:40:51 -0400

    Hamlet MAR1919 17:40 checkpoint

--- src/main/java/HamletParser.java
+++ src/main/java/HamletParser.java
@@ -1,6 +1,8 @@
 import java.io.File;
 import java.io.IOException;
 import java.util.Scanner;
+import java.util.regex.Matcher;
+import java.util.regex.Pattern;
 
 /**
  * Created by thook on 10/7/15.
@@ -9,31 +11,60 @@ public class HamletParser {
 
     private String hamletData;
 
-    public HamletParser(){
+    public HamletParser() {
         this.hamletData = loadFile();
     }
 
-    private String loadFile(){
+    private String loadFile() {
         ClassLoader classLoader = getClass().getClassLoader();
         File file = new File(classLoader.getResource("hamlet.txt").getFile());
         StringBuilder result = new StringBuilder("");
 
-        try(Scanner scanner = new Scanner(file)){
-            while(scanner.hasNextLine()){
+        try (Scanner scanner = new Scanner(file)) {
+            while (scanner.hasNextLine()) {
                 String line = scanner.nextLine();
                 result.append(line).append("\n");
             }
 
             scanner.close();
-        }catch(IOException e){
+        } catch (IOException e) {
             e.printStackTrace();
         }
 
         return result.toString();
     }
 
-    public String getHamletData(){
+    public String getHamletData() {
         return hamletData;
     }
+
+    public void changeHamletToLeon() {
+        hamletData = replace("Hamlet", "Leon", hamletData);
+        hamletData = replace("HAMLET", "LEON", hamletData);
+    }
+
+    public void changeHoratioToTariq() {
+        hamletData = replace("Horatio", "Tariq", hamletData);
+        hamletData = replace("HORATIO", "TARIQ", hamletData);
+    }
+
+    public String replace(String original, String replacement, String data) {
+        Pattern patternToReplace = Pattern.compile(original);
+        Matcher matcher = patternToReplace.matcher(data);
+        return matcher.replaceAll(replacement);
+    }
+
+    public boolean findHamlet(String data) {
+        Pattern pattern1 = Pattern.compile("Hamlet");
+        Pattern pattern2 = Pattern.compile("HAMLET");
+        return (pattern1.matcher(data).find() || pattern2.matcher(data).find());
+    }
+
+    public boolean findHoratio(String data) {
+        Pattern pattern1 = Pattern.compile("Horatio");
+        Pattern pattern2 = Pattern.compile("HORATIO");
+        return (pattern1.matcher(data).find() || pattern2.matcher(data).find());
+    }
+
 
 }
--- src/test/java/HamletParserTest.java
+++ src/test/java/HamletParserTest.java
@@ -1,3 +1,4 @@
+import org.junit.Assert;
 import org.junit.Before;
 import org.junit.Test;
 
@@ -15,17 +16,55 @@ public class HamletParserTest {
 
     @Test
     public void testChangeHamletToLeon() {
+        // Given
+        Assert.assertTrue(hamletParser.findHamlet(hamletText));
+
+        // When
+        hamletParser.changeHamletToLeon();
+
+        // Then
+        Assert.assertFalse(hamletParser.findHamlet(hamletParser.getHamletData()));
     }
+
 
     @Test
     public void testChangeHoratioToTariq() {
+        // Given
+        Assert.assertTrue(hamletParser.findHamlet(hamletText));
+        // When
+        hamletParser.changeHoratioToTariq();
+        // Then
+        Assert.assertFalse(hamletParser.findHoratio(hamletParser.getHamletData()));
+
     }
 
     @Test
     public void testFindHoratio() {
+        // When
+        boolean actualTrue = hamletParser.findHoratio(hamletParser.getHamletData());
+
+        // Then
+        Assert.assertTrue(actualTrue);
+
+        // When
+        hamletParser.changeHoratioToTariq();
+        boolean actualFalse = hamletParser.findHoratio(hamletParser.getHamletData());
+
+        // Then
+        Assert.assertFalse(actualFalse);
     }
 
     @Test
     public void testFindHamlet() {
+        // When
+        boolean actualTrue = hamletParser.findHamlet(hamletParser.getHamletData());
+        // Then
+        Assert.assertTrue(actualTrue);
+        // When
+        hamletParser.changeHamletToLeon();
+        boolean actualFalse = hamletParser.findHamlet(hamletParser.getHamletData());
+
+        // Then
+        Assert.assertFalse(actualFalse);
     }
 }
